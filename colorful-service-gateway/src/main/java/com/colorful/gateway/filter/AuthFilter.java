b668d81d865697dfa1442094f658281139718823
chengdongyi <13088805160@163.com>
2019-11-04 09:59:36 +0800

    SpringCloud Gateway 路由网关

---
+++ colorful-service-gateway/src/main/java/com/colorful/gateway/filter/AuthFilter.java
@@ -0,0 +1,52 @@
+package com.colorful.gateway.filter;
+
+import com.colorful.commons.api.Resp;
+import com.colorful.commons.enums.ResultCode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局过滤器
+ */
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getQueryParams().getFirst("token");
+
+        if (StringUtils.isBlank(token)) {
+            ServerHttpResponse response = exchange.getResponse();
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                byte[] data = objectMapper.writeValueAsBytes(Resp.error(ResultCode.TOKEN_ERROR));
+                // 输出错误信息到页面
+                DataBuffer buffer = response.bufferFactory().wrap(data);
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+                return response.writeWith(Mono.just(buffer));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 设置过滤器的执行顺序
+     * 返回值越小, 优先级越高
+     */
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
